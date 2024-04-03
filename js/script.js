34b80cf21e7828aca0cee1b71181721f6b910b73
Aftiyan <aftiyangamer45@gmail.com>
2024-04-03 10:45:55 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// DEKLARASI VARIABEL 
+
+// var angka = 1; //Menggunakan var (TIDAK DISARANKAN)
+
+//let angka = 1; //Menggunakan let (ISI VARIABEL BISA DIRUBAH)
+// angka = 2;
+
+//const angkaSatuVariabel = 1; // Menggunakan const
+
+//let text = "Hai, ini dari javascript";
+
+// let lima = 5; // TIPE DATA NUMBER
+// let dua = 2;
+
+// let sepuluh  = 10;
+// let tiga = 3;
+
+// let pengurangan = 5 - 5;
+// let tambah = 10 + 5;
+// let kali = 20 * 2;
+// let bagi = 10 / 2;
+
+// console.log("Hasil operasi matematika : ");
+// console.log("Hasil pengurangan = ", pengurangan);
+// console.log("Hasil penambahan = ", tambah);
+// console.log("Hasil perkalian = ", kali);
+// console.log("Hasil pembagian = ", bagi);
+
+
+
+// CONCATENATION (Penggabungan antara variabel)
+// let masBaktiar = "Baktiar";
+// let masMahardika = "Mahardika";
+// let namaLengkap = masBaktiar + " anggep aja ini spasi " + masMahardika;
+
+let tambah = 5 + 3;
+let kurang = 5 - 3;
+let kali = 5 * 3;
+let bagi = 10 / 5;
+
+let hasilTambah = "Hasil tambah = " + tambah;
+let hasilKurang = "Hasil kurang = " + kurang;
+let hasilKali = "Hasil kali = " + kali;
+let hasilBagi = "Hasil bagi = " + bagi;
+
+console.log(hasilTambah);
+console.log(hasilKurang);
+console.log(hasilKali);
+console.log(hasilBagi);
